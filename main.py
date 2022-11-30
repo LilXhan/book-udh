27eaab8baab4fcad1b0934a85acce6536f3876e3
Xhan <alvaradotuctod@gmail.com>
2022-11-30 12:03:26 -0500

    v1.4

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from app import create_app
 from flask import redirect, url_for
 
-app = create_app()
+main = create_app()
 
-@app.errorhandler(404)
+@main.errorhandler(404)
 def index(error):
     return redirect(url_for('app_router.index'))
 
 
 
 if __name__ == "__main__":
-    app.run()
+    main.run()
