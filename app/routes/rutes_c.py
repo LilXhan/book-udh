81e3bdbf55a796a441f1ceca26dfcb105cece2fd
Xhan <alvaradotuctod@gmail.com>
2022-12-02 10:54:54 -0500

    add

--- app/routes/rutes_c.py
+++ app/routes/rutes_c.py
@@ -10,57 +10,68 @@ def c_simples():
         {
             'id': 1,
             'titulo': 'Ejercicio 1',
-            'description': ['Algoritmo que permita el ingreso de dos números enteros y calcule la división de ambos números.']
+            'description': ['Algoritmo que permita el ingreso de dos números enteros y calcule la división de ambos números.'],
+            'link_replit': 'https://replit.com/@bryletucto/1?embed=true'
         },
         {
             'id': 2,
             'titulo': 'Ejercicio 2',
             'description': ['Algoritmo que convierta soles a dólares.'],
+            'link_replit': 'https://replit.com/@bryletucto/2?embed=true'
         },
         {
             'id': 3,
             'titulo': 'Ejercicio 3',
             'description': ['Algoritmo que convierta kilómetros en metros y en centímetros.'],
+            'link_replit': 'https://replit.com/@bryletucto/3?embed=true'
         },
         {
             'id': 4,
             'titulo': 'Ejercicio 4',
             'description': ['Algoritmo que convierta horas en minutos y en segundos.'],
+            'link_replit': 'https://replit.com/@bryletucto/4?embed=true'
         },
         {
             'id': 5,
             'titulo': 'Ejercicio 5',
             'description': ['Algoritmo que convierta grados Celsius a grados Fahrenheit.'],
+            'link_replit': 'https://replit.com/@bryletucto/5?embed=true'
         },
         {
             'id': 6,
             'titulo': 'Ejercicio 6',
             'description': ['Algoritmo que eleve al cubo un número entero.'],
+            'link_replit': 'https://replit.com/@bryletucto/6?embed=true'
         },
         {
             'id': 7,
             'titulo': 'Ejercicio 7',
             'description': ['Algoritmo que calcule la distancia (MRU) sabiendo la velocidad y el tiempo recorrido por un móvil.'],
+            'link_replit': 'https://replit.com/@bryletucto/7?embed=true'
         },
         {
             'id': 8,
             'titulo': 'Ejercicio 8',
             'description': ['Algoritmo que calcule el salario mensual, sabiendo el número de horas trabajadas y el valor por hora.'],
+            'link_replit': 'https://replit.com/@bryletucto/8?embed=true'
         },
         {
             'id': 9,
             'titulo': 'Ejercicio 9',
             'description': ['Un docente desea saber el porcentaje de varones y mujeres que hay en un salón de clases; realice el algoritmo respectivo.'],
+            'link_replit': 'https://replit.com/@bryletucto/9?embed=true'
         },
         {
             'id': 10,
             'titulo': 'Ejercicio 10',
             'description': ['Un alumno desea saber el promedio de sus calificaciones teniendo en cuenta que:', '- El promedio de sus 4 tareas académicas es el 60% del promedio final.', '- El examen de medio curso es el 15% del promedio final.', '- El examen final es el 25% del promedio final.'],
+            'link_replit': 'https://replit.com/@bryletucto/10?embed=true'
         },
         {
             'id': 11,
             'titulo': 'Ejercicio 11',
             'description': ['Algoritmo que calcule el nuevo salario de un obrero, si obtuvo un incremento del 25% sobre su salario anterior.'],
+            'link_replit': 'https://replit.com/@bryletucto/11?embed=true'
         },
     ]
 
